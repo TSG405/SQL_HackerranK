b539eecbdb83b79412e38bd2d5dfc0c2d9199b8f
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-10-28 18:59:32 +0530

    Create Revisiting the SELECT query.sql

---
+++ Revisiting the SELECT query.sql
@@ -0,0 +1,8 @@
+'''
+Problem Statement: https://www.hackerrank.com/challenges/revising-the-select-query/problem
+@Coded by TSG, 2020
+'''
+
+SELECT *
+FROM City
+WHERE CountryCode = 'USA' AND Population >= 100000;
